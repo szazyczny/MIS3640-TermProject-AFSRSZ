8b4a6f36e6786df2528927c802965e88cf85f7d0
szazyczny <35873673+szazyczny@users.noreply.github.com>
2018-11-27 16:22:52 -0500

    Import DB

---
+++ sqlite_demo.py
@@ -0,0 +1,30 @@
+import sqlite3
+db = sqlite3.connect('data.sqlite')
+
+
+for row in db.execute('select * from MuscleGroup'):
+    print(row)
+
+for row in db.execute('select * from Back'):
+    print(row)
+
+for row in db.execute('select * from Abs'):
+    print(row)
+
+for row in db.execute('select * from Duration'):
+    print(row)
+
+for row in db.execute('select * from Chest'):
+    print(row)
+
+for row in db.execute('select * from Legs'):
+    print(row)
+
+for row in db.execute('select * from Arms'):
+    print(row)
+
+for row in db.execute('select * from Stretch'):
+    print(row)
+
+for row in db.execute('select * from CoreBody'):
+    print(row)
