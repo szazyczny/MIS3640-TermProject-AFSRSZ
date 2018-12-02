78ccfe44915f77fa3e12efd55d6d0682dc21d7eb
szazyczny <35873673+szazyczny@users.noreply.github.com>
2018-12-02 17:50:17 -0500

    New Abs List and Dictionary

--- PythonCode/Flask-proj/flasktest.py
+++ PythonCode/Flask-proj/flasktest.py
@@ -10,8 +10,21 @@ app.config['DEBUG'] = True
 
 app.secret_key = "Some secret string here"
 
-d = {'side-plank': "https://kettlebellsworkouts.com/wp-content/uploads/2018/06/36_side_plank_with_extension.png",
-''}
+d = {
+    'Shoulder-Taps':'https://kettlebellsworkouts.com/wp-content/uploads/2018/06/06_shoulder_taps.png',
+    'Bird-Dog':'https://kettlebellsworkouts.com/wp-content/uploads/2018/06/08_bird_dog.png',
+    'Elbow-Knee':'https://kettlebellsworkouts.com/wp-content/uploads/2018/06/12_elbow_to_knee.png',
+    'Hip-Openers':'https://kettlebellsworkouts.com/wp-content/uploads/2018/06/15_hip_openers.png',
+    'Fast-Mountain-Climbers':'https://kettlebellsworkouts.com/wp-content/uploads/2018/06/17_fast_mountain_climbers.png',
+    'Side-Plank-Rotation':'https://kettlebellsworkouts.com/wp-content/uploads/2018/06/18_side_plank_with_rotation.png',
+    'Scissors':'https://kettlebellsworkouts.com/wp-content/uploads/2018/06/22_scissors.png',
+    'High-Knees':'https://kettlebellsworkouts.com/wp-content/uploads/2018/06/24_high_knees.png',
+    'Cross-Overs':'https://kettlebellsworkouts.com/wp-content/uploads/2018/06/25_cross_overs.png',
+    'Dead-Bug':'https://kettlebellsworkouts.com/wp-content/uploads/2018/06/26_dead_bug.png',
+    'Side-Plank':'https://kettlebellsworkouts.com/wp-content/uploads/2018/06/28_side_plank.png',
+    'Cross-Body-Mountain-Climbers':'https://kettlebellsworkouts.com/wp-content/uploads/2018/06/30_cross_body_mountain_climbers.png',
+    'Side-Plank-Extension':'https://kettlebellsworkouts.com/wp-content/uploads/2018/06/36_side_plank_with_extension.png'
+}
 
 @app.route('/<action>')
 def home(action):
