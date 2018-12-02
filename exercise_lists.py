8880b1005213bb94b859d492643b63aab2f51625
szazyczny <35873673+szazyczny@users.noreply.github.com>
2018-12-02 18:08:49 -0500

    Meeting Updates 12/2

--- exercise_lists.py
+++ exercise_lists.py
@@ -2,27 +2,79 @@
 
 import random
 
+# Abs = [
+#     'Bodyweight Shoulder Taps',
+#     'Bodyweight Bird Dog',
+#     'Bodyweight Elbow to Knee',
+#     'Bodyweight Hip Openers',
+#     'Bodyweight Fast Mountain Climbers',
+#     'Bodyweight Side Plank with Rotation',
+#     'Bodyweight Scissors',
+#     'Bodyweight High Knees',
+#     'Bodyweight Cross Overs',
+#     'Bodyweight Dead Bug',
+#     'Bodyweight Side Plank',
+#     'Bodyweight Cross Body Mountain Climbers',
+#     'Bodyweight Side Plank with Extension'
+# ]
+
+# Arms = [
+#     'Bodyweight Dirty Dogs',
+#     'Bodyweight Dive Bomber Push Ups',
+#     'Bodyweight Plank to Push Up',
+#     'Bodyweight Breakdancer Push Ups'
+# ]
+
+# Back = [
+#     'Bodyweight Back Extensions',
+#     'Bodyweight Single Leg Up and Down Dogs',
+#     'Bodyweight Cross Body Extension'
+# ]
+
+# Chest = [
+#     'Bodyweight Push Ups',
+#     'Bodyweight T Push Ups',
+#     'Bodyweight Mountain Climber Push Ups'
+# ]
+
+# Legs = [
+#     'Bodyweight Y Squats',
+#     'Bodyweight Single Leg Deadlift',
+#     'Bodyweight Bridge',
+#     'Bodyweight Forward Lunge',
+#     'Bodyweight Lateral Squat',
+#     'Bodyweight Static Lunge',
+#     'Bodyweight Yoga Squat',
+#     'Bodyweight Single Leg Deadlift with Overhead Reach',
+#     'Bodyweight Burpees',
+#     'Bodyweight Jump Squats',
+#     'Bodyweight Reverse Lunge and Hop',
+#     'Bodyweight Jumping Lunges',
+#     'Bodyweight Single Leg Burpee'
+# ]
+
+
 Abs = [
-    'Bodyweight Shoulder Taps',
-    'Bodyweight Bird Dog',
-    'Bodyweight Elbow to Knee',
-    'Bodyweight Hip Openers',
-    'Bodyweight Fast Mountain Climbers',
-    'Bodyweight Side Plank with Rotation',
-    'Bodyweight Scissors',
-    'Bodyweight High Knees',
-    'Bodyweight Cross Overs',
-    'Bodyweight Dead Bug',
-    'Bodyweight Side Plank',
-    'Bodyweight Cross Body Mountain Climbers',
-    'Bodyweight Side Plank with Extension'
+    'Shoulder-Taps',
+    'Bird-Dog',
+    'Elbow-Knee',
+    'Hip-Openers',
+    'Fast-Mountain-Climbers',
+    'Side-Plank-Rotation',
+    'Scissors',
+    'High-Knees',
+    'Cross-Overs',
+    'Dead-Bug',
+    'Side-Plank',
+    'Cross-Body-Mountain-Climbers',
+    'Side-Plank-Extension'
 ]
 
 Arms = [
-    'Bodyweight Dirty Dogs',
-    'Bodyweight Dive Bomber Push Ups',
-    'Bodyweight Plank to Push Up',
-    'Bodyweight Breakdancer Push Ups'
+    'Dirty-Dogs',
+    'Dive-Bomber-Push-Ups',
+    'Plank-Push-Up',
+    'Breakdancer-Push-Ups'
 ]
 
 Back = [
