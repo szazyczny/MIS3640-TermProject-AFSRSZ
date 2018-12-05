7d814861efaabb9997fa5689adf01f02491aa3b4
szazyczny <35873673+szazyczny@users.noreply.github.com>
2018-12-05 17:08:49 -0500

    Office Hours

--- Flask-timer/timer-master/src/timerapp.py
+++ Flask-timer/timer-master/src/timerapp.py
@@ -4,9 +4,12 @@
 import re
 
 from flask import Flask, render_template, url_for, redirect, request, flash
+import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'really secret'
+
+
 
 d = {
     # Abs
@@ -52,15 +55,79 @@ d = {
     'Single-Leg-Burpee':'https://kettlebellsworkouts.com/wp-content/uploads/2018/06/50_single_leg_burpee.png'
 }
 
+Abs = [
+    'Shoulder-Taps',
+    'Bird-Dog',
+    'Elbow-Knee',
+    'Hip-Openers',
+    'Fast-Mountain-Climbers',
+    'Side-Plank-Rotation',
+    'Scissors',
+    'High-Knees',
+    'Cross-Overs',
+    'Dead-Bug',
+    'Side-Plank',
+    'Cross-Body-Mountain-Climbers',
+    'Side-Plank-Extension'
+]
+
+Arms = [
+    'Dirty-Dogs',
+    'Dive-Bomber-Push-Ups',
+    'Plank-Push-Up',
+    'Breakdancer-Push-Ups'
+]
+
+Back = [
+    'Back-Extensions',
+    'Single-Leg-Up-Down-Dogs',
+    'Cross-Body-Extension'
+]
+
+Chest = [
+    'Push-Ups',
+    'T-Push-Ups',
+    'Mountain-Climber-Push-Ups'
+]
+
+Legs = [
+    'Y-Squats',
+    'Single-Leg-Deadlift',
+    'Bridge',
+    'Forward-Lunge',
+    'Lateral-Squat',
+    'Static-Lunge',
+    'Yoga-Squat',
+    'Single-Leg-Deadlift-Overhead-Reach',
+    'Burpees',
+    'Jump-Squats',
+    'Reverse-Lunge-Hop',
+    'Jumping-Lunges',
+    'Single-Leg-Burpee'
+]
+
+
+UpperBody = Arms + Back + Chest
+
+CoreBody = Abs
+
+LowerBody = Legs
+
+FullBody = UpperBody + CoreBody + LowerBody
+
+    
 @app.route('/')
 def index():
-    return redirect(url_for('timer', num=25 * 60))
+    return redirect(url_for('timer', num=1500, action='Bird-Dog'))
 
 
-@app.route('/<int:num>s')
-@app.route('/<int:num>')
-def timer(num):
-    return render_template('index.html', num=num)
+@app.route('/<int:num>s/<action>')
+@app.route('/<int:num>/<action>')
+def timer(num, action=None):
+    if action is None:
+        action = 'Shoulder-Taps'
+    actionImage = d[action]
+    return render_template('index.html', num=num, action=action, actionImage=actionImage)
 
 
 @app.route('/custom', methods=['GET', 'POST'])
@@ -72,7 +139,7 @@ def custom():
         flash('Please enter a valid time duration, for example, 34, 20s, 15m, 2h')
         return redirect(url_for('index'))
     if time[-1] not in 'smh':
-        return redirect(url_for('timer', num=int(time)))
+        return redirect(url_for('timer', num=int(time), action='Bird-Dog'))
     else:
         types = {'s': 'timer', 'm': 'minutes', 'h': 'hours'}
         return redirect(url_for(types[time[-1]], num=int(time[:-1])))
@@ -80,19 +147,58 @@ def custom():
 
 @app.route('/<int:num>m')
 def minutes(num):
-    return redirect(url_for('timer', num=num * 60))
+    return redirect(url_for('timer', num=num * 60, action='Bird-Dog'))
 
 
 @app.route('/<int:num>h')
 def hours(num):
-    return redirect(url_for('timer', num=num * 3600))
+    return redirect(url_for('timer', num=num * 3600, action='Bird-Dog'))
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     flash('Error! ')
-    return redirect(url_for('timer', num=244))
+    return redirect(url_for('timer', num=244, action='Bird-Dog'))
 
+
+import time
+
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        t -= 1
+    print('Next!\n\n')
+
+@app.route('/exercise')
+def demo(muscle=FullBody, duration='ten'):
+    counter = 0
+    if duration == 'ten':
+        while counter < 10:
+            action = random.choice(muscle)
+            print(action)
+            return redirect(url_for('timer', num=5, action=action))
+            counter += 1
+            countdown(5)
+        else:
+            print('YAY, U NOT FAT!')
+    elif duration == 'twenty':
+        while counter < 20:
+            print(random.choice(muscle))
+            counter += 1
+            countdown(2)
+        else:
+            print('YAY, U NOT FAT!')
+    else:
+        while counter < 30:
+            print(random.choice(muscle))
+            counter += 1
+            countdown(2)
+        else:
+            print('YAY, U NOT FAT!')
 
 
 if __name__ == '__main__':
---
+++ Flask-timer/timer-master/src/timerappsarah.py
@@ -0,0 +1,58 @@
+# $env:FLASK_APP = "timerappsarah.py"
+# -*- coding: utf-8 -*-
+
+import re
+
+from flask import Flask, render_template, url_for, redirect, request, flash
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'really secret'
+
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('timer', num=25 * 60))
+
+
+@app.route('/<int:num>s')
+@app.route('/<int:num>')
+def timer(num):
+    return render_template('index.html', num=num)
+
+
+@app.route('/custom', methods=['GET', 'POST'])
+def custom():
+    time = request.form.get('time', 180)
+    # use re to validate input data
+    m = re.match('\d+[smh]?$', time)
+    if m is None:
+        flash('Please enter a valid time duration, for example, 34, 20s, 15m, 2h')
+        return redirect(url_for('index'))
+    if time[-1] not in 'smh':
+        return redirect(url_for('timer', num=int(time)))
+    else:
+        types = {'s': 'timer', 'm': 'minutes', 'h': 'hours'}
+        return redirect(url_for(types[time[-1]], num=int(time[:-1])))
+
+
+@app.route('/<int:num>m')
+def minutes(num):
+    return redirect(url_for('timer', num=num * 60))
+
+
+@app.route('/<int:num>h')
+def hours(num):
+    return redirect(url_for('timer', num=num * 3600))
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    flash('Error! ')
+    return redirect(url_for('timer', num=244))
+
+
+
+
+if __name__ == '__main__':
+    app.run()
